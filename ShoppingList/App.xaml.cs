9e522395316d5ff029850d13e1356d0fc88b2f80
Tyler Hanson <700211854@fvtc.edu>
2026-04-14 15:06:02 -0500

    Initial Creation of ShoppingList Assignment (Login, Logout, Create Account)

---
+++ ShoppingList/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingList.Views;
+namespace ShoppingList;
+
+public partial class App : Application
+{
+    public static string SessionKey = "";
+    
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage( new MainPage());
+    }
+}
